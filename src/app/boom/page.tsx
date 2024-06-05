cb5d91c8198b42891a83262d43bf407ec47c6801
lucascvarani-ciandt <162370781+lucascvarani-ciandt@users.noreply.github.com>
2024-06-05 20:56:07 -0300

    Redirect user to the leaderboard after boom

--- src/app/boom/page.tsx
+++ src/app/boom/page.tsx
@@ -4,21 +4,43 @@ import { useEffect, useState } from "react";
 import WithAuthentication from "../middleware/WithAuthentication";
 import { ScheduledBoom } from "../controllers/BoomController";
 import Confetti from "react-confetti";
-import { useWindowSize } from "usehooks-ts";
+import { useInterval, useWindowSize } from "usehooks-ts";
+import moment from "moment-timezone";
+import { useRouter } from "next/navigation";
 
 const Boom = () => {
   const [scheduledBoom, setScheduleBoom] = useState<ScheduledBoom | null>(null);
 
   const { width, height } = useWindowSize();
 
+  const router = useRouter();
+
   useEffect(() => {
     const newScheduledBoom = localStorage.getItem("scheduledBoom");
-    if (newScheduledBoom === null) {
+    if (!newScheduledBoom) {
       setScheduleBoom(null);
     } else {
       setScheduleBoom(JSON.parse(newScheduledBoom));
     }
   }, []);
+
+  const goToLeaderboardPage = () => {
+    router.replace("/leaderboard");
+  };
+
+  const checkBoomTimer = () => {
+    const endDate = moment(scheduledBoom?.endDate).tz("America/Sao_Paulo");
+    const now = moment().tz("America/Sao_Paulo");
+    const diff = endDate.diff(now);
+    if (diff < 0) {
+      localStorage.setItem("scheduledBoom", "");
+      goToLeaderboardPage();
+    }
+  };
+
+  useInterval(() => {
+    checkBoomTimer();
+  }, 1000);
 
   if (!scheduledBoom) {
     return <div>LOADING BOOM</div>;

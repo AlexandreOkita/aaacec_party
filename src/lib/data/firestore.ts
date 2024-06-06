f1e2b8e1b6021a620e4e3c0b0fb50b429bc68d2e
AlexandreOkita <xande.okita@gmail.com>
2024-06-05 21:43:24 -0300

    use firestoreCredentials from envvar

--- src/lib/data/firestore.ts
+++ src/lib/data/firestore.ts
@@ -1,8 +1,18 @@
 import { Firestore } from "@google-cloud/firestore";
 
+var firestoreCredentials;
+if (process.env.FIRESTORE_CREDENTIALS) {
+  firestoreCredentials = JSON.parse(process.env.FIRESTORE_CREDENTIALS);
+} else if (process.env.AAACEC_PARTY_ENVIRONMENT == "production") {
+  throw new Error("Firestore credentials not found.");
+}
+
 const firestore = new Firestore({
   projectId: "aaacecparty",
-  keyFilename: "src/lib/data/firebase_credentials.json",
+  credentials: {
+    client_email: firestoreCredentials.client_email,
+    private_key: firestoreCredentials.private_key,
+  },
 });
 
 if (process.env.AAACEC_PARTY_ENVIRONMENT !== "production") {

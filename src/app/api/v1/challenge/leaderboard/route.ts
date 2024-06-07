f65bd5d944578d8f9daa8b02b6415c52352aedfc
AlexandreOkita <xande.okita@gmail.com>
2024-06-07 20:37:16 -0300

    add cache control in leaderboard route

--- src/app/api/v1/challenge/leaderboard/route.ts
+++ src/app/api/v1/challenge/leaderboard/route.ts
@@ -3,7 +3,17 @@ import { ChallengeRepository } from "../../../../repositories/challenge_reposito
 class LeaderboardController {
   static async GET(_request: Request) {
     const scores = await ChallengeRepository.getAllScores();
-    return Response.json({ scores: scores }, { status: 200 });
+    return Response.json(
+      { scores: scores },
+      {
+        status: 200,
+        headers: {
+          "Cache-Control": "max-age=0, s-maxage=0",
+          "CDN-Cache-Control": "max-age=0, s-maxage=0",
+          "Vercel-CDN-Cache-Control": "max-age=0, s-maxage=0",
+        },
+      }
+    );
   }
 }
 

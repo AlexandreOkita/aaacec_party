ed060428182f027b72440388d76512f9ea14f382
AlexandreOkita <xande.okita@gmail.com>
2024-06-07 22:51:29 -0300

    add firebase hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  headers: () => [
+    {
+      source: "/:path*",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store",
+        },
+      ],
+    },
+  ],
   env: {
     JWT_SECRET: process.env.JWT_SECRET,
   },

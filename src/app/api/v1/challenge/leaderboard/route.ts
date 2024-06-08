13b33830ceee0eb4a229c7e2f781ce32ee418f19
AlexandreOkita <xande.okita@gmail.com>
2024-06-07 23:04:09 -0300

    try making leadearboard non static route

--- src/app/api/v1/challenge/leaderboard/route.ts
+++ src/app/api/v1/challenge/leaderboard/route.ts
@@ -1,24 +1,32 @@
+import { NextRequest } from "next/server";
 import { ChallengeRepository } from "../../../../repositories/challenge_repository";
+import { APIError } from "../../../../../lib/error/api_error";
+import { Authorize } from "../../../../../lib/route_method";
+import { AAACECRole } from "../../../../domain/aaacec_roles";
 
 class LeaderboardController {
-  static async GET(_request: Request) {
-    const scores = await ChallengeRepository.getAllScores();
-    return Response.json(
-      { scores: scores },
-      {
-        status: 200,
-        headers: {
-          "Cache-Control": "max-age=0, s-maxage=0",
-          "CDN-Cache-Control": "max-age=0, s-maxage=0",
-          "Vercel-CDN-Cache-Control": "max-age=0, s-maxage=0",
-        },
-      }
-    );
+  @Authorize([AAACECRole.ADMIN])
+  static async GET(req: NextRequest) {
+    if (req.nextUrl.searchParams.has("tid")) {
+      const scores = await ChallengeRepository.getAllScores();
+      return Response.json(
+        { scores: scores },
+        {
+          status: 200,
+          headers: {
+            "Cache-Control": "max-age=0, s-maxage=0",
+            "CDN-Cache-Control": "max-age=0, s-maxage=0",
+            "Vercel-CDN-Cache-Control": "max-age=0, s-maxage=0",
+          },
+        }
+      );
+    }
+    throw new APIError("Please provide tid as a query parameter.", 400);
   }
 }
 
-export const fetchCache = 'force-no-store';
+export const fetchCache = "force-no-store";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   return LeaderboardController.GET(request);
 }
